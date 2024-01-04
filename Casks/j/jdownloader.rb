d33c25828958bfb986ebdd41dc96067238549b74
Bo Anderson <mail@boanderson.me>
2024-01-04 06:08:25 +0000

    jdownloader: update URLs to newer installer

--- Casks/j/jdownloader.rb
+++ Casks/j/jdownloader.rb
@@ -1,9 +1,13 @@
 cask "jdownloader" do
-  version "48254"
-  sha256 :no_check
+  arch arm: "aarch64", intel: "amd64"
 
-  url "https://installer.jdownloader.org/clean/JD2Setup.dmg",
-      user_agent: :fake
+  version "48254"
+  # we only download an installer so sha256 rarely changes
+  sha256 arm:   "f0f8a1fa0e0de3c85a1a8d10030e2ccb9a04ed2be7818490500f99b77b29e6da",
+         intel: "c7d7ba17e80881c31023b7c94f6bffab4ef9a6103f0423d08cfd909be2fe859e"
+
+  # 17.0.x version here is JDK version - not jdownloader version
+  url "https://installer.jdownloader.org/homebrew/JDownloader2Setup_macos-#{arch}_v17_0_3.dmg"
   name "JDownloader"
   desc "Download manager"
   homepage "https://jdownloader.org/"
@@ -16,7 +20,7 @@ cask "jdownloader" do
   auto_updates true
 
   preflight do
-    system_command "#{staged_path}/JDownloader Installer.app/Contents/MacOS/JavaApplicationStub",
+    system_command "#{staged_path}/JDownloader 2 Installer.app/Contents/MacOS/JavaApplicationStub",
                    args:         [
                      "-dir", appdir.to_s,
                      "-q",
